c9d3b980ec2404c690700fb0ad8a2e893d2e6383
howsilence <statton0104@gmail.com>
2022-03-01 17:26:58 -0500

    commit tree a56440a26ee129b38ba24d977cba7b90e77004c2

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,18 @@
 class Audition < ActiveRecord::Base
-  
+    #DELIVERABLE ONE
+    belongs_to :role
+
+    #DELIVERABLE TWO
+    def self.role
+        self.role
+    end
+
+    def self.call_back
+        self.update(hired: true)
+    end
+
+
+
+
+
 end
---
+++ app/models/role.rb
@@ -0,0 +1,16 @@
+class Role < ActiveRecord::Base
+
+    #FIRST DELIVERABLE
+    has_many :auditions
+
+    #THIRD DELIVERABLES
+    def self.auditions
+        self.auditions
+    end
+
+    
+
+
+
+
+end
